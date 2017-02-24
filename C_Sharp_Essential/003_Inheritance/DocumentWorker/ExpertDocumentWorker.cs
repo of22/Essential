dcfbe256cd5d2f52564c4dbff0628fa71bf73015
Orest Filonenko <of@nustay.com>
2017-02-24 13:35:16 +0200

    Finished DocumentWorker task

---
+++ C_Sharp_Essential/003_Inheritance/DocumentWorker/ExpertDocumentWorker.cs
@@ -0,0 +1,12 @@
+﻿namespace DocumentWorker
+{
+    using System;
+
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
---
+++ C_Sharp_Essential/003_Inheritance/DocumentWorker/ProDocumentWorker.cs
@@ -0,0 +1,17 @@
+﻿namespace DocumentWorker
+{
+    using System;
+
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}
